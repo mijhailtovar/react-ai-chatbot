4e6c12e3e3bc0da5fceb5c2102e0bdea476e51a2
mijhail <mijhailtovar@gmail.com>
2026-08-08 06:50:09 -0400

    clase 25 aplicando autofocus

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,12 +1,23 @@
 // esto permite a un texarea o contenedor ser autodimensionable
 // es el efecto de deepseek cuando metes mucho texto el input crece automaticamente
 import TextareaAutosize from 'react-textarea-autosize';
+// se usa el use effect y use ref para poder manejar referencias de estado
+// del texarea y manejar el autofocus con 'Ref'
+import { useEffect, useRef } from "react";
 
 import { useState } from "react";
 import styles from "./Controls.module.css";
 
 export function Controls({ is_disable = false ,onSend }) {
+    const textarea_ref = useRef(null); // almacenara el estado del textarea
     const [content, setContent] = useState("");
+
+    //si el desabilitador cambia de un valor a false le da el focus
+    useEffect(() => {
+        if (!is_disable) {
+            textarea_ref.current.focus();
+        }
+    }, [is_disable]);
 
     function handleContentChange(event) {
         setContent(event.target.value);
@@ -31,8 +42,9 @@ export function Controls({ is_disable = false ,onSend }) {
         <div className={styles.Controls}>
             <div className={styles.TextAreaContainer}>
                 <TextareaAutosize 
+                    ref={textarea_ref} //autofocus despues de recibir respuesta
                     className={styles.TextArea} 
-                    disabled={is_disable}
+                    disabled={is_disable} //se desavilita despues de mandar un mensaje hasta que reciba respuesta
                     placeholder="Message AI Chatbot" 
                     value={content}
                     onChange={handleContentChange}
