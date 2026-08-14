cfa8b42766a835c4390f457f047359f71e41593d
mijhail <mijhailtovar@gmail.com>
2026-08-14 06:17:47 -0400

    se personalizo el loader con estilos css para una animacion original

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,11 @@
 // src/components/Loader/Loader.jsx
+import styles from './Loader.module.css'; // ← Importas los estilos
+
 export function Loader() {
-    return (
-      <div className="flex justify-center items-center py-4">
-        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-500"></div>
-        <span className="ml-2 text-gray-400">Pensando...</span>
-      </div>
-    );
-  }
+  return (
+    <div className="flex justify-center items-center py-4">
+      {/* Aplicas la clase del CSS Module */}
+      <div className={styles.loader}></div>
+    </div>
+  );
+}
